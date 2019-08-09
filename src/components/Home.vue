7f01a698a7f83994a38961565e542ad95b0173a9
Settakit0515 <44047548+Settakit0515@users.noreply.github.com>
2019-08-09 17:33:59 +0700

    Add files via upload

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
       <v-layout row wrap>
         <v-flex xs12 sm4 md3 v-for="i in 10" :key="i" class="px-3 pb-3">
           <v-card class="px-5 py-2">
-            <v-layout row wrap>
+            <v-layout row wrap class="pb-2">
               <v-flex xs4>
                 <div class="centerdiv setcenter">
                   <img
@@ -20,6 +20,14 @@
                 <p>Time: 183 min.</p>
               </v-flex>
             </v-layout>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-layout row wrap>
+                <v-flex xs3 sm4 md4 v-for="i in 6" :key="i" class="pa-1">
+                  <v-btn class="grey darken-1" block text>00.00</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-card-actions>
           </v-card>
         </v-flex>
       </v-layout>
